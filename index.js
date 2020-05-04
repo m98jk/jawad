505e31af4de1bf16ad9a5aa99fcec4a5a62512fc
m98jk <al7joker111@gmail.com>
2020-05-04 06:26:15 +0300

    remove console.log

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-function jawad(n) {
+module.exports.jawad = function jawad(n) {
   return n.split('').reverse().join('');
-}
-console.log(jawad('any string need to reverse'));
-
-module.exports.jawad = jawad;
+};
